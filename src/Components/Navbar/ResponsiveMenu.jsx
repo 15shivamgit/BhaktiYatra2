c71e3153f403839383db3a1b3903f5cf130da161
15shivambgs <15shivambgs@gmail.com>
2025-11-15 20:16:04 +0530

    update resposive

--- src/Components/Navbar/ResponsiveMenu.jsx
+++ src/Components/Navbar/ResponsiveMenu.jsx
@@ -22,12 +22,13 @@ const ResponsiveMenu = ({ showMenu, setShowMenu }) => {
                         <Link to='/tours'><li onClick={()=>setShowMenu(false)}>Tours</li></Link>
                         <Link to='/gallery'><li onClick={()=>setShowMenu(false)}>Gallery</li></Link>
                         <Link to='/contact'><li onClick={()=>setShowMenu(false)}>Contact</li></Link>
+                        <Link to='/login'><li onClick={()=>setShowMenu(false)}>Login</li></Link>
                         <Link to='#'><button onClick={()=>setShowMenu(false)} className='bg-red-500 text-white px-4 py-1 rounded-md font-semibold'>Book Now</button></Link>
                     </ul>
                 </nav>
             </div>
             <div>
-                <h1>Made with ❤️ by Rohit</h1>
+                <h1>Made with ❤️ by Shivam</h1>
             </div>
         </div>
     )
